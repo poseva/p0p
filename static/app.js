fe90e7dc2dda769fc0444bb1c468574baeb334d3
Sebastian Pop (uidp3416) <sebastian.pop@continental-corporation.com>
2022-09-06 18:09:57 +0300

    add IFC viewer

---
+++ static/app.js
@@ -0,0 +1,28 @@
+import { Color } from 'three';
+import { IfcViewerAPI } from 'web-ifc-viewer';
+
+const container = document.getElementById('viewer-container');
+const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0xffffff) });
+viewer.grid.setGrid();
+viewer.axes.setAxes();
+
+async function loadIfc(url) {
+    await viewer.IFC.setWasmPath("IFC/");
+    const model = await viewer.IFC.loadIfcUrl(url);
+    viewer.shadowDropper.renderShadow(model.modelID);
+}
+
+loadIfc('../CasaR_complet.ifc');
+
+window.ondblclick = () => viewer.IFC.selector.pickIfcItem(true);
+window.onmousemove = () => viewer.IFC.selector.prePickIfcItem();
+viewer.clipper.active = true;
+
+window.onkeydown = (event) => {
+    if(event.code === 'KeyP') {
+        viewer.clipper.createPlane();
+    }
+    else if(event.code === 'KeyO') {
+        viewer.clipper.deletePlane();
+    }
+}
