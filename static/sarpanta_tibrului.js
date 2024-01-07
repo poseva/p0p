b57577ebea248e8e56c7e424f10aaaa2d1ab7ea1
SePo <seba@p0p.ro>
2024-01-07 17:37:28 +0200

    prepare for sarpanta_tibrului ifc

--- static/sarpanta_tibrului.js
+++ static/sarpanta_tibrului.js
@@ -113721,7 +113721,7 @@
      viewer.shadowDropper.renderShadow(model.modelID);
  }
  
- loadIfc('https://p0p.ro/CasaR_complet.ifc');
+ loadIfc('https://p0p.ro/sarpanta_tibrului.ifc');
  
  window.ondblclick = () => viewer.IFC.selector.pickIfcItem(true);
  window.onmousemove = () => viewer.IFC.selector.prePickIfcItem();
